3c27f3fd3550f742bcdc405003b82122d4fbe776
AlexeyKalyadin <akalyadin@astralinux.ru>
2021-03-19 20:28:12 +0300

    start

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git init
+git config --global user.email "akalyadin@astralinux.ru"
+git config --global user.name "AlexeyKalyadin"
+git remote add origin https://github.com/akalyadin/exam.git
+git pull https://github.com/akalyadin/exam.git main
+#ssh-keygen -t rsa -N "" -f ./id_rsa
+#cat id_rsa.pub
+./web.sh
